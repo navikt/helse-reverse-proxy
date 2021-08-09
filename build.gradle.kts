f579bab88d45aecc56c4ab652ea91bea93b029c7
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-08-09 13:42:09 +0200

    Oppgraderer dusseldorf-ktor, Gradle wrapper, plugins og actions. (#36)
    - Legger til fuel med fuel-coroutines da det er fjernet fra dusseldorf-ktor.
    - Fjerner utgåtte repositories.
    - Closes #24
    - Closes #28
    - Closes #31
    - Closes #35

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,32 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
-val dusseldorfKtorVersion = "1.5.2.fa18872"
+val dusseldorfKtorVersion = "2.1.6.2-6ce5eaa"
+val fuelVersion = "2.3.1"
+
 val mainClass = "no.nav.helse.AppKt"
 
 plugins {
-    kotlin("jvm") version "1.4.31"
-    id("com.github.johnrengelman.shadow") version "6.1.0"
+    kotlin("jvm") version "1.5.21"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 
 }
 
 buildscript {
-    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/fa1887241239fdd1877f0f258a94872281db8783/gradle/dusseldorf-ktor.gradle.kts")
+    apply("https://raw.githubusercontent.com/navikt/dusseldorf-ktor/6ce5eaa4666595bb6b550fca5ca8bbdc242961a0/gradle/dusseldorf-ktor.gradle.kts")
 }
 
 dependencies {
     implementation ( "no.nav.helse:dusseldorf-ktor-core:$dusseldorfKtorVersion")
     implementation ( "no.nav.helse:dusseldorf-ktor-client:$dusseldorfKtorVersion")
+    implementation("com.github.kittinunf.fuel:fuel:$fuelVersion")
+    implementation("com.github.kittinunf.fuel:fuel-coroutines:$fuelVersion"){
+        exclude(group = "org.jetbrains.kotlinx", module = "kotlinx-coroutines-core")
+    }
 }
 
 repositories {
-    maven("https://dl.bintray.com/kotlin/ktor")
-    maven("https://kotlin.bintray.com/kotlinx")
+    mavenLocal()
 
     maven {
         name = "GitHubPackages"
@@ -32,8 +37,6 @@ repositories {
         }
     }
 
-    jcenter()
-    mavenLocal()
     mavenCentral()
 }
 
@@ -61,5 +64,5 @@ tasks.withType<ShadowJar> {
 }
 
 tasks.withType<Wrapper> {
-    gradleVersion = "6.7.1"
+    gradleVersion = "7.1.1"
 }
